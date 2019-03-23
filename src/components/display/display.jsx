82ad887600613d3d4c5b2ee082422e44d78cec52
arsonite <notarson@gmail.com>
2019-03-23 17:58:24 +0100

    trying to implement tab-to-edit

--- src/components/display/display.jsx
+++ src/components/display/display.jsx
@@ -4,9 +4,6 @@ import ToolBar from './toolBar';
 
 import './style/display.css';
 
-const FLAG_NL = '';
-const FLAG_S = '';
-
 class Display extends Component {
   word = string => {
     let p = document.createElement('p');
@@ -16,24 +13,22 @@ class Display extends Component {
   };
 
   package = text => {
-    text = text.replace(/<p.+?>/g, '');
-    text = text.replace(/<\/p>/g, '');
+    text = text.replace(/<p.+?>|<\/p>|<\/div>|<span.+?>|<\/span>/g, '');
+    text = text.replace(/<div.+?>/g, '§');
     text = text.replace(/<br>/g, '');
-    text = text.replace(/<div>/g, '§');
-    text = text.replace(/<\/div>/g, '');
-    text = text.replace(/&nbsp;/g, ' ');
+    text = text.replace(/&nbsp;/g, '');
 
     text = text.split(/[\s§]/g);
-    console.log(text);
     let packagedText = document.createElement('div');
 
+    // TODO: Convert to for-int since I have to look up if next symbol is a dot, for sentence structure to avoid space, trim if not
     for (let string of text) {
       if (string === '§') {
         packagedText.innerHTML += '\n';
         continue;
       }
 
-      packagedText.appendChild(this.word(string));
+      packagedText.appendChild(this.word(string + ' '));
     }
     return packagedText.innerHTML;
   };
