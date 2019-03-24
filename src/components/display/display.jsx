1964e3929612cfac4e666b494bdd4cbddfbdb0bc
arsonite <notarson@gmail.com>
2019-03-24 12:24:42 +0100

    started working on v0.2

--- src/components/display/display.jsx
+++ src/components/display/display.jsx
@@ -3,34 +3,50 @@ import React, { Component } from 'react';
 import ToolBar from './toolBar';
 
 import './style/display.css';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+
+const FLAG_STOPPER = 'Φ';
+const FLAG_NL = 'Ψ';
+const FLAG_S = 'Ω';
 
 class Display extends Component {
+  state = {
+    selector: undefined
+  };
+
   word = string => {
     let p = document.createElement('p');
     p.className = 'word';
     p.innerHTML = string;
+    p.onclick = e => {
+      console.log(e.target.style);
+    };
+
     return p;
   };
 
   package = text => {
-    text = text.replace(/<p.+?>|<\/p>|<\/div>|<span.+?>|<\/span>/g, '');
-    text = text.replace(/<div.+?>/g, '§');
-    text = text.replace(/<br>/g, '');
-    text = text.replace(/&nbsp;/g, '');
+    text = text.replace(/<p.+?>|<\/p>|<\/div>|<span.+?>|<\/span>|<br>/g, '');
+    text = text.replace(/<div>/g, FLAG_STOPPER + FLAG_NL);
+    text = text.replace(/&nbsp;|\s/g, FLAG_S + ' ');
 
-    text = text.split(/[\s§]/g);
+    text = text.split(/[\sΦΩ]/g);
     let packagedText = document.createElement('div');
+    packagedText.id = 'content';
 
     // TODO: Convert to for-int since I have to look up if next symbol is a dot, for sentence structure to avoid space, trim if not
     for (let string of text) {
-      if (string === '§') {
-        packagedText.innerHTML += '\n';
+      if (string === FLAG_NL) {
+        packagedText.appendChild(document.createElement('br'));
+        continue;
+      } else if (string === '') {
+        packagedText.innerHTML += ' ';
         continue;
       }
-
-      packagedText.appendChild(this.word(string + ' '));
+      packagedText.appendChild(this.word(string));
     }
-    return packagedText.innerHTML;
+    this.setState({ selector: 1 });
+    return packagedText;
   };
 
   render() {
@@ -38,11 +54,14 @@ class Display extends Component {
       <div id='display' className='screen'>
         <ToolBar />
         <main
-          contentEditable='true'
+          contentEditable={this.state.selector === undefined ? true : false}
           onBlur={e => {
-            e.target.innerHTML = this.package(e.target.innerHTML);
+            let main = this.package(e.target.innerHTML);
+            e.target.innerHTML = '';
+            e.target.appendChild(main);
           }}
         />
+        <div id='content' />
       </div>
     );
   }
